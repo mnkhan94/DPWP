f0dbfbe7539134908f415815ab7250790925e3a1
Nabeel Khan <rockystorm@gmail.com>
2016-07-14 18:35:59 -0700

    Added Dynamic Web Page response through variable storing

--- simple-login/main.py
+++ simple-login/main.py
@@ -8,30 +8,31 @@ Simple Form
 import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
-    def get(self): # function that starts everything. Catalyst.
-        page = '''<!DOCTYPE HTML>
+    def get(self):
+        header = '''<!DOCTYPE html>
 <html>
-	<head>
-		<title>Simple Form</title>
-	</head>
-	<body>
-		<form method="GET">
-			
-			<label>Name: <input type="text" name="user" />
-			<label>Email: <input type="text" name="email" />
-			<input type="submit" value="Submit" />
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>'''
 
-		</form>
-	</body>
-</html>'''
-	if self.request.GET:
-		user = self.request.GET['user']
-		email = self.request.GET['email']
 
-		print "Hi " + user + "!"
+        body = '''<form method="GET">
+        <label>Name:</label><input type="text" name="user" />
+        <label>Email:</label><input type="email" name="email" />
+        <input type="submit" value="Submit" />'''
 
-        self.response.write(page) #PRINT 
+        footer = '''</form>
+    </body>
+</html>
+        '''
 
+        if self.request.GET:
+            user = self.request.GET["user"]
+            email = self.request.GET["email"]
+            self.response.write(header + "Thank You, " + user + "! We sent a confirmation email to: " + email + "<hr>" + body + footer)
+        else:
+            self.response.write(header+body+footer)
 
 #never touch this. This is just magic.
 app = webapp2.WSGIApplication([
