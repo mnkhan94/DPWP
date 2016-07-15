ea29310754cefcf7c3ab26b847a9772b7bf522f3
Nabeel Khan <rockystorm@gmail.com>
2016-07-14 18:04:59 -0700

    Added Form and two inputs

--- simple-login/main.py
+++ simple-login/main.py
@@ -2,19 +2,28 @@
 Mohammad Khan
 07/11/16
 DPfWP
-Setting Up Launcher
+Simple Form
 '''
 
 import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): # function that starts everything. Catalyst.
-        self.response.write('Hello world!')
-        #code goes here
+        page = '''<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Simple Form</title>
+	</head>
+	<body>
+		<form method="GET">
+			
+			<label>Name: <input type="text" />
+			<label>Email: <input type="text" />
 
-    def additional_functions(self):
-    	pass
-    	#code goes here
+		</form>
+	</body>
+</html>'''
+        self.response.write(page)
 
 #never touch this. This is just magic.
 app = webapp2.WSGIApplication([
