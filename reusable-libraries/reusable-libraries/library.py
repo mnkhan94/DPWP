c22eadd832ebc9c20b1bc31e7442bcf8924a60ba
Nabeel Khan <rockystorm@gmail.com>
2016-07-21 16:33:07 -0700

    Added dummy content for testing Favorite songs lsit

--- reusable-libraries/reusable-libraries/library.py
+++ reusable-libraries/reusable-libraries/library.py
@@ -1,53 +1,58 @@
+class FavoriteSongs(object):
+    def __init__(self):
+        self.__song_list = []
+
+    def addSong(self, m):
+        self.__song_list.append(m)
+
+    def compileList(self):
+        output = ""
+        for song in self.__song_list: #for eachs song in song array
+            output += "Title: " + song.title + "<br /><small>"+song.artist+"</small>"
+        return output
+
+
 class Song(object): #Data Object
-	def __init__(self):
-		self.__title = "" #Validate Title
-		self.__artist = ""
-		self.__rating = "" #Validate Rating
-		self.__length = "" #Validate Length
-		self.__genre = ""
-		self.__link = ""
+    def __init__(self):
+        self.__title = "" #Validate Title
+        self.__artist = ""
+        self.__rating = "" #Validate Rating
+        self.__genre = ""
+        self.__link = ""
 
-# Song Title getter/setter
-@property
-def title(self):
-    return self.__title
+    # Song Title getter/setter
+    @property
+    def title(self):
+        return self.__title
 
-@title.setter
-def title(self, y):
-    self.__title = y
+    @title.setter
+    def title(self, y):
+        self.__title = y
 
-# Song Artist getter/setter
-@property
-def artist(self):
-    return self.__artist
+    # Song Artist getter/setter
+    @property
+    def artist(self):
+        return self.__artist
 
-@artist.setter
-def artist(self, y):
-    self.__artist = y
+    @artist.setter
+    def artist(self, y):
+        self.__artist = y
 
-# Song Rating getter/setter
-@property
-def rating(self):
-    return self.__rating
+    # Song Rating getter/setter
+    @property
+    def rating(self):
+        return self.__rating
 
-@rating.setter
-def rating(self, y):
-    self.__rating = y
+    @rating.setter
+    def rating(self, y):
+        self.__rating = y
 
-# Song Length getter/setter
-@property
-def length(self):
-    return self.__length
 
-@rating.setter
-def rating(self, y):
-    self.__length = y
+    # Song Artist getter/setter
+    @property
+    def genre(self):
+        return self.__genre
 
-# Song Artist getter/setter
-@property
-def rating(self):
-    return self.__rating
-
-@rating.setter
-def rating(self, y):
-    self.__rating = y
+    @rating.setter
+    def genre(self, y):
+        self.__genre = y
--- reusable-libraries/reusable-libraries/main.py
+++ reusable-libraries/reusable-libraries/main.py
@@ -11,10 +11,27 @@ from library import *
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-		
-		p = FormPage()
+        
+        p = FormPage()
 
-		self.response.write(p.print_out())
+        lib = FavoriteSongs()
+
+        # 
+
+        s1 = Song()
+        s1.title = "This is what you came for"
+        s1.artist = "Calvin Harris"
+        s1.rating = "5"
+        s1.genre = "Pop"
+        s1.link = "https://www.youtube.com/watch?v=Ybo4QvKVHoE"
+        lib.addSong(s1)
+
+
+        # 
+
+
+        p.body = lib.compileList()
+        self.response.write(p.print_out())
 
 
 app = webapp2.WSGIApplication([
